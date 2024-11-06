78e3a0d2b04012354d2b20fb2bde4c5807b653e3
Shehbaz <mdshehbaz666666@gmail.com>
2024-11-06 13:31:17 +0530

    add server js

--- server/server.js
+++ server/server.js
@@ -9,30 +9,27 @@ const adminRoute = require("./router/admin-router");
 const { connectDB } = require("./utils/DB");
 const errorMiddleware = require('./middlewares/error-middleware');
 let port =  process.env.PORT || 8000;
-// const corsOptions = {
-//   origin: '*',
-//   method:"POST GET PUT PATCH DELETE HEAD",
-//   credentials: true
-// }
+const corsOptions = {
+  origin: '*',
+  method:"POST GET PUT PATCH DELETE HEAD",
+  credentials: true
+}
 
 
 // middleware 
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
 // Mount the Router : To use the router in your main express app, you can "mount" it at a specific URL prefix
-// app.use("/api/auth", authRoute);
-// app.use("/api/form", contactRoute);
-// app.use("/api/data", serviceRoute);
-// app.use("/api/admin", adminRoute);
-// app.use(errorMiddleware)
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute);
+app.use("/api/data", serviceRoute);
+app.use("/api/admin", adminRoute);
+app.use(errorMiddleware)
 
-// connectDB().then(() => {
-//   app.listen(port, () => {
-//     console.log("Active port 8000");
-//   });
-// });
-app.listen(port, () => {
-  console.log("Active port 8000");
+connectDB().then(() => {
+  app.listen(port, () => {
+    console.log("Active port 8000");
+  });
 });
